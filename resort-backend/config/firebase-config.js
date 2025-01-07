7157b6d7d6a5a4a9c76e812b0ee581d643ee5131
Nikhiln5k <nikhilskumar823@gmail.com>
2025-01-07 20:03:26 +0530

    firebase credential update

--- resort-backend/config/firebase-config.js
+++ resort-backend/config/firebase-config.js
@@ -1,10 +1,22 @@
 const admin = require("firebase-admin");
 
 // Use your Firebase project's service account key
-const serviceAccount = require("./resort-web-app-firebase-adminsdk-etjuh-26c775fc00.json");
+const firebaseConfig = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfig),
 });
 
 module.exports = admin;
